a0e7d378cd68ebbfe876946e8e9224e5c057f534
cpnv <cpnv@cpnv.ch>
2015-05-07 09:04:37 +0200

    test 1

---
+++ test/formulaire.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+          <meta charset="UTF-8">
+          <title>Formulaire php</title>
+      </head>
+    <body>
+        <h1>Choisissez un chiffre</h1>
+        
+        
+        <!--pour l'envoi du formulaire à resultat.php-->
+        <form name="form1" method="post" action="resultat.php">
+            
+            
+            <label>
+                <p>choisissez un chiffre entre 2 et 16 </p>
+                
+                
+                <!--où l'utilisateur va entrer le nombre-->
+                <input type="number" name="nombre" >
+                
+                
+                <!--bouton submit-->
+                <input type="submit" name="test" value="Tester"> 
+            </label>
+        </form>
+    </body>
+</html>
---
+++ test/resultat.php
@@ -0,0 +1,82 @@
+<?php
+/*déclaration des variables*/
+$nombre= $_POST['nombre'];
+
+
+?>
+
+<html>
+	<head>
+		<link rel="stylesheet" href="style.css" />
+		<meta charset="UTF-8">
+		<title>résultat</title>
+	</head>
+	<body>
+		<table border=1> 
+
+		<?php
+		echo "<h1>Table de multiplication en base $nombre</h1>"; // Nom du tableau
+
+		echo "<tr><th>x</th>"; // Cellule en haut à gauche
+
+
+		for($i=0;$i<$nombre;$i++) // variables pour les colonnes
+			echo "<th>$i</th>";
+
+			echo "</tr>\n";
+
+
+			for($j=0;$j<$nombre;$j++) // variables pour les lignes
+			{
+		 	 	echo "<tr><th>$j</th>";
+		 		 for($i=0;$i<$nombre;$i++)
+
+		  	{
+
+
+				$hex= ($j*$i);
+				$convert = base_convert($hex, 10, $nombre); //Convertit les nombres en base du nombre choisi
+
+		    	echo "<td>".$convert."</td>"; // Affichage dans les cellules
+		  }
+		}
+		?>
+
+		</table>
+		<br>
+		<table border=1> 
+		<?php
+		echo "<h1>Table d'addition en base $nombre</h1>"; // Titre du tableau
+
+		echo "<tr><th>+</th>"; // Cellule haut gauche
+		
+        for($i=0;$i<$nombre;$i++)// variables pour les colonnes
+			echo "<th>$i</th>";
+			echo "</tr>\n";
+
+
+			for($j=0;$j<$nombre;$j++) // variables pour les lignes
+			{
+
+			  echo "<tr><th>$j</th>";
+				  for($i=0;$i<$nombre;$i++)
+				  {
+
+					$hex= ($j+$i);
+					$convert = base_convert($hex, 10, $nombre);//Convertit les nombres en base du nombre choisi
+
+				    echo "<td>".$convert."</td>"; // Affichage dans les cellules
+				     
+
+				}
+
+			}
+
+
+		?>
+
+		</table>
+	<body>
+</html>
+
+
